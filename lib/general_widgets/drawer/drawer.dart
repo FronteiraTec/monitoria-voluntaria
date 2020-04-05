2bd96badecfd70ed63b563fee83bf6c0a737b184
fersasil <gfersasil@gmail.com>
2020-04-04 22:37:10 -0300

    UI: gradiente adicionado, ui alterada

--- lib/general_widgets/drawer/drawer.dart
+++ lib/general_widgets/drawer/drawer.dart
@@ -2,25 +2,44 @@ import 'package:flutter/material.dart';
 import './drawer_widgets/drawer_head.dart';
 import './drawer_widgets/drawer_link.dart';
 
-
 import 'package:monitoring/screens/Login/sign_in_screen.dart';
 
 class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.7, //20.0,
+      width: MediaQuery.of(context).size.width * 0.86, //20.0,
       child: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            MyDrawerHead(),
-            DrawerLink(name: "Monitorias", routeName: "/", icon: Icons.book),
-            DrawerLink(name: "Oi", routeName: "/oi"),
-            DrawerLink(name: "Link 3", routeName: "/"),
-            DrawerLink(name: "Link 4", routeName: "/"),
-            DrawerLink(name: "Entrar", routeName: SignInScreen.routeName, icon: Icons.verified_user),
-          ],
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.only(),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                MyDrawerHead(),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: (MediaQuery.of(context).size.width * 0.38) / 2),
+                  // padding: EdgeInsets.only(top: 10),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      DrawerLink(name: "Monitorias", routeName: "/", icon: Icons.book),
+                      DrawerLink(name: "Oi", routeName: "/oi"),
+                      DrawerLink(name: "Link 3", routeName: "/"),
+                      DrawerLink(name: "Link 4", routeName: "/"),
+                      DrawerLink(
+                          name: "Entrar",
+                          routeName: SignInScreen.routeName,
+                          icon: Icons.verified_user),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/general_widgets/drawer/drawer_widgets/drawer_head.dart
+++ lib/general_widgets/drawer/drawer_widgets/drawer_head.dart
@@ -7,36 +7,51 @@ class MyDrawerHead extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .35,
+    return Container(
+      decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+               // 10% of the width, so there are ten blinds.
+              colors: [
+                const Color(0xFF398783),
+                const Color(0xFF1b582e)
+              ], // whitish to gray
+              // repeats the gradient over the canvas
+            ),
+            
+          ),
+      height: MediaQuery.of(context).size.height * .33,
       child: DrawerHeader(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Expanded(child: UserPhoto()),
-            Divider(
+            Container(width: 110, height: 110, child: UserPhoto()),
+            SizedBox(
               height: 20,
             ),
 
             Text(
               username,
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 25,
+                fontWeight: FontWeight.w400,
                 color: Colors.white,
               ),
             ),
 
+            SizedBox(height: 2),
             Container(
               margin: EdgeInsets.only(top: 3.5),
               child: Text(
                 "@$userId",
-                style: TextStyle(color: Colors.white),
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
               ),
             ),
           ],
         ),
-        decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+        // decoration: BoxDecoration(color: Theme.of(context).primaryColor),
       ),
     );
   }
